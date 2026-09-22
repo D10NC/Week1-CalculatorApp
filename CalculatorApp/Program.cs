eab30703737c5baf22a1ef6a9b03635bc2f9948e
Dion <B01015811@M-MXB105C51>
2026-09-22 11:57:17 +0100

    Implemented program code.

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type in the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type in the second");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives you {2}", firstNumber, secondNumber, result);
+Console.ReadKey();
