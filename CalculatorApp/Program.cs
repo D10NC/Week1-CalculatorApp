bd90cc794e2ded4ef39f7a597706c8128a5c5c68
Dion <B01015811@M-MXB105C51>
2026-09-22 12:00:13 +0100

    Update

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -4,8 +4,6 @@ int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Type in the second");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-
-
 int result = firstNumber + secondNumber;
 
 Console.WriteLine("Adding {0} and {1} gives you {2}", firstNumber, secondNumber, result);
